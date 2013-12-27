00b47494fa76d0e594af4b98dc34d16d17da522e
Colby Aley <colby@aley.me>
2013-12-26 19:03:36 -0800

    Photos work

--- commitphotos.gemspec
+++ commitphotos.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   # Dependencies
   spec.add_runtime_dependency "mini_magick"
   spec.add_runtime_dependency "choice"
+  spec.add_runtime_dependency "rest-client"
 
   # Development dependencies
   spec.add_development_dependency "bundler", "~> 1.3"
--- lib/commitphotos/commands/image.rb
+++ lib/commitphotos/commands/image.rb
@@ -1,4 +1,27 @@
+require 'rest-client'
+
 module Commitphotos
   def image
+    filename = "/tmp/#{Time.now.to_i}.jpg"
+    message = `git log -1 HEAD --pretty=format:%s`
+     
+    begin
+      `imagesnap -q -w 3 #{filename}`
+      `convert #{filename} -resize '800x800>' #{filename}`
+     
+      RestClient.post('http://commitphotos.herokuapp.com/photos/new', {
+        "email" => `git config --get user.email`.chomp,
+        "user_name" => `git config --get user.name`.chomp,
+        "photo" => File.open(filename)
+        }
+      )
+     
+      FileUtils.rm(filename)
+      exit 0
+    rescue => e
+      puts "there was an error: #{e.message}"
+      FileUtils.rm(filename)
+      exit 1
+    end
   end
 end
