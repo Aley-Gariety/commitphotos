69fe1e4d5bd33aab9bd689222ef13cc64aeed5a5
Jackson Gariety <personal@jacksongariety.com>
2013-12-26 18:02:49 -0800

    Starts writing the video command.

--- lib/commitphotos/commands/video.rb
+++ lib/commitphotos/commands/video.rb
@@ -1,4 +1,15 @@
+require "mini_magick"
+require "rest_client"
+
 module Commitphotos
   def video
+    filename = "/tmp/#{Time.now}.mov"
+    `#{__FILE__}/../../videosnap -t 2 --no-audio #{filename}`
+    
+    video = MiniMagick::Image.open filename
+    vide.convert :gif
+    
+    
+    RestClient.new()
   end
 end
